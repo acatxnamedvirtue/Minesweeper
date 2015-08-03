18fc5b57072e9f0dadec0778133c5fd267125411
acatxnamedvirtue <tsb252@gmail.com>
2015-08-03 14:04:57 -0400

    Add Board#populate

--- board.rb
+++ board.rb
@@ -1,3 +1,6 @@
+require 'byebug'
+require_relative 'tile'
+
 class Board
   def initialize(width = 9, height = 9)
     @grid = Array.new(width) { Array.new(height) }
@@ -12,4 +15,20 @@ class Board
     x, y = pos
     grid[x][y] = tile
   end
+
+  def populate
+    rows.each_with_index do |row, row_idx|
+      row.each_with_index do |_, col_idx|
+        pos = [row_idx, col_idx]
+        self[pos] = Tile.new(self, pos)
+      end
+    end
+  end
+
+  def rows
+    grid
+  end
+
+  private
+  attr_reader :grid
 end

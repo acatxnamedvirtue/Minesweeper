3ab0025a629768522bb21139693a6633e5dd1a67
acatxnamedvirtue <tsb252@gmail.com>
2015-08-03 13:53:08 -0400

    Add Board#[] and Board#[]= methods

--- board.rb
+++ board.rb
@@ -2,4 +2,14 @@ class Board
   def initialize(width = 9, height = 9)
     @grid = Array.new(width) { Array.new(height) }
   end
+
+  def [](pos)
+    x, y = pos
+    grid[x][y]
+  end
+
+  def []=(pos, tile)
+    x, y = pos
+    grid[x][y] = tile
+  end
 end

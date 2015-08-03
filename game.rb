c1ca6909e0565ebc30b615122bf55e9a0bc451f1
acatxnamedvirtue <tsb252@gmail.com>
2015-08-03 16:03:51 -0400

    Fix run errors

--- game.rb
+++ game.rb
@@ -7,18 +7,22 @@ class Game
   end
 
   def run
-    until board.over?
+    board.populate
+    board.add_bombs
 
+    until board.over?
+      puts board.render
+      make_move
     end
   end
 
   def make_move
     pos, action = nil, nil
 
-    until valid_move?(pos, action)
+    #until valid_move?(pos, action)
       pos = get_pos
       action = get_action
-    end
+    #end
 
     case action
     when :f then board.flag_tile(pos)
@@ -34,7 +38,7 @@ class Game
 
     until valid_pos?(pos)
       puts "Please enter a position x, y"
-      pos = gets.chomp.split(",")
+      pos = gets.chomp.split(",").map(&:to_i)
     end
 
     pos
@@ -52,10 +56,16 @@ class Game
   end
 
   def valid_pos?(pos)
+    return false unless pos
     board.in_range?(pos)
   end
 
   def valid_action?(action)
     [:f, :r].any? { |sym| sym == action }
   end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  game = Game.new
+  game.run
 end

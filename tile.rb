14dacbb4720288c1de495c9f3ca3c58e2eb6f20d
acatxnamedvirtue <tsb252@gmail.com>
2015-08-03 12:11:03 -0400

    Finish Tile attributes

--- tile.rb
+++ tile.rb
@@ -10,6 +10,8 @@ class Tile
     [+1, +1]
 ]
 
+  attr_writer :bomb, :flagged
+
   def initialize(board, pos)
     @board = board
     @pos = pos
@@ -18,10 +20,23 @@ class Tile
     @revealed = false
   end
 
+  def bomb?
+    bomb
+  end
+
+  def flagged?
+    flagged
+  end
+
+  def revealed?
+    revealed
+  end
+
   def reveal
     revealed = true
   end
 
   private
+  attr_reader :board, :pos, :bomb, :flagged
   attr_accessor :revealed
 end

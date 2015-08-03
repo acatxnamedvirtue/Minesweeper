c475d5068c869f9f822d26b6d9eb10eff1e126cc
acatxnamedvirtue <tsb252@gmail.com>
2015-08-03 15:27:35 -0400

    Add Game#get_move and helper methods

--- game.rb
+++ game.rb
@@ -11,5 +11,39 @@ class Game
   end
 
   def get_move
+    pos = get_pos
+    action = get_action
+  end
+
+  def get_pos
+    pos = nil
+
+    until valid_pos?(pos)
+      puts "Please enter a position x, y"
+      pos = gets.chomp.split(",")
+    end
+
+    pos
+  end
+
+  def get_action
+    action = nil
+
+    until valid_action?(action)
+      puts "Please enter an action, e.g. toggle (F)lag, (R)eveal"
+      action = gets.chomp[0].downcase.to_sym
+    end
+
+    action
+  end
+
+  private
+
+  def valid_pos?(pos)
+    board.in_range?(pos)
+  end
+
+  def valid_action?(action)
+    [:f, :r].any? { |sym| sym == action }
   end
 end

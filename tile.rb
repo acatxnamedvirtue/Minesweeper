68f2cfad895c311d34acba1ff7faf57877c43aa1
acatxnamedvirtue <tsb252@gmail.com>
2015-08-03 12:00:50 -0400

    Add neighbor offsets to Tile

--- tile.rb
+++ tile.rb
@@ -1,4 +1,15 @@
 class Tile
+  NEIGHBOR_OFFSETS = [
+    [-1, -1],
+    [+0, -1],
+    [+1, -1],
+    [-1, +0],
+    [-1, +0],
+    [-1, +1],
+    [+0, +1],
+    [+1, +1]
+]
+
   def initialize(board, pos)
     @board = board
     @pos = pos
